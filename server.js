b9d8ebd4d6939e872d06e5d50cfab7e534ab1b2e
Surya D <surya.devireddy.004@gmail.com>
2024-08-12 18:48:32 +0530

    ..

--- server.js
+++ server.js
@@ -1,44 +1,46 @@
 // app.js
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const studentDataModel = require('./models/studentData');
-const studentQueryModel = require('./models/studentQuery');
-const AdminData = require('./models/adminData');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const studentDataModel = require("./models/studentData");
+const studentQueryModel = require("./models/studentQuery");
+const AdminData = require("./models/adminData");
 
 const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://surya-007:abcd1234@cluster0.e95bgae.mongodb.net/SmartFacilityManagementSystem', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("", { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 // Check MongoDB connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
 // Middleware to parse incoming requests
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use session middleware
-app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "your-secret-key", resave: true, saveUninitialized: true })
+);
 
 // Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 // Routes
-app.get('/', (req, res) => {
-  res.redirect('/login');
+app.get("/", (req, res) => {
+  res.redirect("/login");
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.post('/student/login', async (req, res) => {
+app.post("/student/login", async (req, res) => {
   const { registerNo, password } = req.body;
 
   try {
@@ -49,38 +51,35 @@ app.post('/student/login', async (req, res) => {
         registerNo: student.registerNo,
         name: student.name,
         phoneNo: student.phoneNo,
-        studentid: student._id
+        studentid: student._id,
       };
 
-      res.render('studentHome', { student });
+      res.render("studentHome", { student });
     } else {
       res.send("Invalid credentials");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.get('/student/register', (req, res) => {
-  res.render('student-register');
+app.get("/student/register", (req, res) => {
+  res.render("student-register");
 });
 
-app.get('/admin/register', (req, res) => {
+app.get("/admin/register", (req, res) => {
   // Render the admin registration form using EJS
-  res.render('adminRegistration');
+  res.render("adminRegistration");
 });
 
-
-
-
-app.post('/student/register', async (req, res) => {
+app.post("/student/register", async (req, res) => {
   const { registerNo, password, phoneNo, name } = req.body;
   const student = new studentDataModel({
     registerNo,
     password,
     name,
-    phoneNo
+    phoneNo,
   });
 
   try {
@@ -90,57 +89,52 @@ app.post('/student/register', async (req, res) => {
       registerNo: student.registerNo,
       name: student.name,
       phoneNo: student.phoneNo,
-      studentid: student._id
+      studentid: student._id,
     };
 
-    res.redirect('/student/home');
+    res.redirect("/student/home");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.get('/student/home', (req, res) => {
-
+app.get("/student/home", (req, res) => {
   const data = req.session.student;
-  const student =  studentDataModel.findOne(data.registerNo);
-  res.render('studentHome', { student });
+  const student = studentDataModel.findOne(data.registerNo);
+  res.render("studentHome", { student });
 });
 
-
-app.post('/admin/register', (req, res) => {
+app.post("/admin/register", (req, res) => {
   // Extract admin registration details from request body
   const { registerNo, password } = req.body;
 
   // Create a new admin data document
   const newAdmin = new AdminData({
-      adminid: registerNo,
-      password: password
+    adminid: registerNo,
+    password: password,
   });
 
   // Save the admin data to the database
-  newAdmin.save()
-      .then(() => {
-          console.log('Admin registered successfully');
-           res.redirect('/admin/home');
-      })
-      .catch(err => {
-          console.error('Error registering admin:', err);
-          res.status(500).send('Error registering admin');
-      });
+  newAdmin
+    .save()
+    .then(() => {
+      console.log("Admin registered successfully");
+      res.redirect("/admin/home");
+    })
+    .catch((err) => {
+      console.error("Error registering admin:", err);
+      res.status(500).send("Error registering admin");
+    });
 });
 
-
-
-
-
-app.post('/student/raisequery', async (req, res) => {
+app.post("/student/raisequery", async (req, res) => {
   const { querytitle, query } = req.body;
   const student = req.session.student;
 
   try {
     if (!student) {
-      return res.status(401).send('Unauthorized');
+      return res.status(401).send("Unauthorized");
     }
 
     // Retrieve the student's ID from the session
@@ -153,12 +147,9 @@ app.post('/student/raisequery', async (req, res) => {
       // Add the new query to the student's queries array
       foundStudent.queries.push({
         querytitle: querytitle,
-        query: query
+        query: query,
       });
       await foundStudent.save();
-
-
-
 
       const newStudentQuery = new studentQueryModel({
         name: foundStudent.name,
@@ -168,34 +159,29 @@ app.post('/student/raisequery', async (req, res) => {
         studentid: foundStudent._id,
       });
       // Save the new studentQuery document to the database
-       newStudentQuery.save();
-       res.render('studentHome',{student : foundStudent});
+      newStudentQuery.save();
+      res.render("studentHome", { student: foundStudent });
     } else {
-      res.status(404).send('Student not found');
+      res.status(404).send("Student not found");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-
-
-
-app.get('/admin/home', async (req, res) => {
+app.get("/admin/home", async (req, res) => {
   try {
     // Retrieve all data from studentQueryModel
     const allQueries = await studentQueryModel.find();
 
     // Render the admin home view and pass the retrieved data
-    res.render('adminHome', { queries: allQueries });
+    res.render("adminHome", { queries: allQueries });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
-
-
 
 // Start the server
 app.listen(port, () => {
